7c25e7cb3891ffb56b208b738ef2641695ab285f
AmonMunyai <Amonmunyai11@gmail.com>
2023-03-27 19:50:46 +0200

    [added] toogleModal function, for interacting with and switching between modals

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 "use strict";
+
+function toggleModal(modalName) {
+    // Toggles the visibility of login, sign up and forgot password popups.
+    let modalAction;
+    const modal = document.querySelectorAll(".modal");
+
+    modal.forEach((item) => {
+        item.classList.remove("is-active");
+    })
+
+    if (modalName != "close") {
+        modalAction = document.getElementById(`modal-${modalName}`);
+        modalAction.classList.add("is-active");
+    }
+}
