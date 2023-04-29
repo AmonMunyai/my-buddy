5259b8fbbdd505fd5167ed3c3c5bd8761183f62d
AmonMunyai <Amonmunyai11@gmail.com>
2023-04-29 03:28:03 +0200

    Implemented timeago format on time, Changed upvote styling from arrow to thumb

--- post/question.php
+++ post/question.php
@@ -1,9 +1,9 @@
 <?php
     require_once "../core/init.php";
+    require_once "../core/functions.php";
 
     if (isset($_GET['id'])) {
         $id = (string) $_GET['id'];
-
 
         $sql = "SELECT * FROM post WHERE id=?";
         $stmt = mysqli_stmt_init($connection);
@@ -56,7 +56,7 @@
 
                     <!-- main content, question and answers -->
                     <div class="content">
-                        <div class="question" id="question-<?php echo $post['id'];?>">
+                        <div class="question" id="question-<?php echo $post['id']; ?>">
                             <div class="question-content">
                                 <div class="question-content-header">
                                     <div class="question-content-title">
@@ -73,18 +73,17 @@
                                 <div class="question-content-footer">
                                     <?php
                                         if (isset($_SESSION['user_id']) && $_SESSION['user_username'] == $post['post_created_by']) {
-                                            echo '<button id="add-comment-btn" class="button is-small" onclick="toggleModal(' . "'add-comment'" . ')">Add a comment</button>';
+                                            echo '<button id="add-comment-btn" class="button is-small" onclick="toggleModal(' . "'add-answer'" . ')">Add a comment</button>';
                                             echo '<button id="add-comment-btn" class="button is-small align-left" onclick="toggleModal(' . "'update-post'" . ')">Edit post</button>';                                            
                                         } else if (isset($_SESSION['user_id'])) {
-                                            echo '<button id="add-comment-btn" class="button is-small" onclick="toggleModal(' . "'add-comment'" . ')">Add a comment</button>';
+                                            echo '<button id="add-comment-btn" class="button is-small" onclick="toggleModal(' . "'add-answer'" . ')">Add a comment</button>';
                                         } else {
                                             echo '<button id="add-comment-btn" class="button is-small" onclick="toggleModal(' . "'login'" . ')">Add a comment</button>';
                                         }
                                     ?>
                                     <div class="user-card">
                                         <div class="user-card-profile-pic">
-                                            <?php $post_avatar = $post['post_created_by_avatar'] ?? ROOT_URL . "/public/assets/images/blank-profile-picture.png"; ?>
-                                            <img src="<?php echo $post_avatar?>" loading="lazy" alt>
+                                            <img src="<?php echo $post['post_created_by_avatar']; ?>" loading="lazy" alt>
                                         </div>
                                         <div class="user-card-info">
                                             <div class="user-card-link">
@@ -92,8 +91,8 @@
                                             </div>
                                         </div>
                                         <time class="user-card-time">
-                                            asked on
-                                            <span class="relative-time text-color-uj"><?php echo $post['post_created_date']; ?></span>
+                                            asked
+                                            <span class="relative-time text-color-uj"><?php echo time_elapsed_string($post['post_created_date']); ?></span>
                                         </time>
                                     </div>
                                 </div>
@@ -128,11 +127,10 @@
                                     <div class="answer-content-header">
                                         <div class="user-card-info">
                                             <div class="user-card-profile-pic">
-                                                <?php $post_answer_avatar = $post_answer['post_answer_created_by_avatar'] ?? ROOT_URL . "/public/assets/images/blank-profile-picture.png"; ?>
-                                                <img src="<?php echo $post_answer_avatar ?>" loading="lazy" alt>
+                                                <img src="<?php echo $post_answer['post_answer_created_by_avatar']; ?>" loading="lazy" alt>
                                             </div>
                                             <div class="user-card-link">
-                                                <h4 class="text-weight-bold"><a href="#" class="text-color-uj"><?php echo $post_answer['post_answer_created_by']?></a></h4>
+                                                <h4 class="text-weight-bold"><a href="#" class="text-color-uj"><?php $post_answer['post_answer_created_by']; ?></a></h4>
                                             </div>
                                         </div>
                                         <div class="answer-vote">
@@ -146,9 +144,9 @@
                                                 $sql = "SELECT * FROM post_answer_vote WHERE post_id=" . $post_answer['id'];
                                                 $post_answer_number_of_votes = mysqli_num_rows(mysqli_query($connection, $sql));
                                             ?>
-                                            <a id="upvote-btn" <?php echo $redirect . "&vote=up"; ?> class="fa fa-arrow-up voting-button"></a>
+                                            <a id="upvote-btn" <?php echo $redirect . "&vote=up"; ?> class="fa fa-thumbs-up voting-button"></a>
                                             <div class="answer-score text-muted text-size-small"><?php echo $post_answer_number_of_votes?></div>
-                                            <a id="downvote-btn" <?php echo $redirect . "&vote=down"; ?> class="fa fa-arrow-down voting-button"></a>
+                                            <a id="downvote-btn" <?php echo $redirect . "&vote=down"; ?> class="fa fa-thumbs-down voting-button"></a>
                                         </div>
                                     </div>
                                     <div class="spacing-block padding-small"></div>
@@ -160,20 +158,26 @@
                                     <div class="spacing-block padding-small"></div>
                                     <div class="answer-content-footer">
                                         <div class="user-card">
-                                            answered on
+                                            answered
                                             <time class="user-card-time text-muted">
-                                                <span><?php echo $post_answer['post_answer_created_date']?></span>
+                                                <span><?php echo time_elapsed_string($post_answer['post_answer_created_date']); ?></span>
                                             </time>
                                         </div>
                                         <?php
                                         if (isset($_SESSION['user_id'])) {
-                                            echo '<button id="add-comment-btn" class="button is-small">reply</button>';
-                                        } else {
-                                            echo '<button id="add-comment-btn" class="button is-small" onclick="toggleModal(' . "'login'" . ')">reply</button>';
+                                            echo '<button id="add-comment-btn" class="button is-small" onclick="toggleModal(' . "'add-comment'" . ')">reply</button>';
                                         }
                                         ?>
-                                        
                                     </div>
+
+                                    <!-- <div class="spacing-block padding-xsmall"></div> -->
+
+                                    <!-- <div class="line-separator-wrapper">
+                                        <div class="line-divider"></div>
+                                        <h5 class="line-separator">comments</h5>
+                                        <div class="line-divider"></div>
+                                    </div> -->
+                                    
                                     <!-- <ul class="answer-content-comments text-size-xsmall">
                                         <li class="answer-comment">
                                             <div class="comment-vote">
