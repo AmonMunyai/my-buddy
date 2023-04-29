03d136fd28c4f79b564890b6e1c5dff29bfe9a77
AmonMunyai <Amonmunyai11@gmail.com>
2023-04-29 03:26:19 +0200

    Added way to know if user wants to receive notifications via email

--- post/create_post.php
+++ post/create_post.php
@@ -5,19 +5,22 @@
         $title = $_POST['title'];
         $content = $_POST['content'];
         $created_by = $_SESSION['user_username'];
-        $number_of_answers = 0;
+        $created_by_id = $_SESSION['user_id'];
+        $created_by_avatar = $_SESSION['user_avatar'];
+
+        $notification_email_status = $_POST['mailsend'] ?? 0;
 
         if (!preg_match("/^[a-zA-Z0-9_]+(?:\W+[a-zA-Z0-9_]+)*\W*$/", $title)) {
             header("Location: " . ROOT_URL . "/dashboard.php?error=invalidtopic&content=".$content);
             exit();
         } else {
-            $sql = "INSERT INTO post (post_title, post_content, post_created_date, post_created_by, post_number_of_answers) VALUES (?, ?, now(), ?, ?)";
+            $sql = "INSERT INTO post (post_title, post_content, post_created_date, post_created_by, post_created_by_id, post_created_by_avatar, post_number_of_answers, post_notification_email_status) VALUES (?, ?, now(), ?, ?, ?, 0, ?)";
             $stmt = mysqli_stmt_init($connection);
             if (!mysqli_stmt_prepare($stmt, $sql)) {
                 header("Location: " . ROOT_URL . "/index.php?error=sqlerror");
                 exit();
             } else {
-                mysqli_stmt_bind_param($stmt, "ssss", $title, $content, $created_by, $number_of_answers);
+                mysqli_stmt_bind_param($stmt, "ssssss", $title, $content, $created_by, $created_by_id, $created_by_avatar, $notification_email_status);
                 mysqli_stmt_execute($stmt);
                 header("Location: " . ROOT_URL . "/dashboard.php?createpost=success");
                 exit();
