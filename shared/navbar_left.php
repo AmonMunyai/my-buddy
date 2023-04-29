d2141255994515dc453f5f635d7dcdd7cbd83f2f
AmonMunyai <Amonmunyai11@gmail.com>
2023-04-29 03:35:53 +0200

    Improved user display name, and Added way to notify when user has notifications

--- shared/navbar_left.php
+++ shared/navbar_left.php
@@ -1,9 +1,15 @@
 <?php
     if (isset($_SESSION['user_firstname']) && isset($_SESSION['user_lastname'])) {
-        $user_display_name = $_SESSION['user_firstname'] . " " . $_SESSION['user_lastname'];
-        $user_display_page = 'href="#"'; // redirect to user profile page
+        $user_display_name = ucfirst($_SESSION['user_firstname']) . " " . ucfirst($_SESSION['user_lastname']);
+
+        $sql = "SELECT user_avatar FROM user_account WHERE id = " . $_SESSION['user_id'];
+        $result = mysqli_query($connection, $sql);
+        $user = mysqli_fetch_assoc($result);
+
+        $user_avatar = $user['user_avatar'];
+        $user_display_page = 'onclick="toggleModal(' . "'profile'" . ')"';
     } else {
-        $user_display_name = "guest";
+        $user_display_name = "Guest";
         $user_display_page = 'onclick="toggleModal(' . "'login'" . ')"';
     }
 ?>
@@ -30,18 +36,31 @@
                                         $redirect = 'href="' . ROOT_URL . '/dashboard.php"';
                                     }
                                 ?>
-<li><a <?php echo $redirect; ?> class="sidebar-link"><i class="fa fa-home"></i>Home</a></li>
+                                <li><a <?php echo $redirect; ?> class="sidebar-link"><i class="fa fa-home"></i>Home</a></li>
                                 <li><a href="#" class="sidebar-link" onclick="toggleModal('search')"><i class="fa fa-search"></i>Search </a></li>
                                 <!-- <li><a href="#" class="sidebar-link"><i class="fa fa-compass"></i>Explore</a></li> -->
                                 <?php
                                     if ($user_display_name !== "guest") {
-                                        echo '<li><a href="#" class="sidebar-link" onclick="toggleModal(' . "'notifications'" . ')"><i class="fa fa-bell"></i>Notifications</a></li>';
+                                        $sql = "SELECT * FROM user_notification WHERE user_id=" . $_SESSION['user_id'];
+                                        $notifications = mysqli_query($connection, $sql);
+                
+                                        if (mysqli_num_rows($notifications) > 0) {        
+                                ?>
+                                <li><a href="#" class="sidebar-link text-color-uj" onclick="toggleModal('notifications')"><i class="fa fa-bell"></i>Notifications</a></li>
+                                <?php
+                                        } else {
+                                ?>
+                                <li><a href="#" class="sidebar-link" onclick="toggleModal('notifications')"><i class="fa fa-bell""></i>Notifications</a></li>
+                                <?php
+                                        }
                                     }
                                 ?>
-<li><a href="#" class="sidebar-link"><i class="fa fa-users"></i>Communities</a></li>
+                                <li><a href="#" class="sidebar-link"><i class="fa fa-users"></i>Communities</a></li>
                                 <?php
                                     if ($user_display_name !== "guest") {
-                                        echo '<li><a href="#" class="sidebar-link" onclick="toggleModal(' . "'create-post'" . ')"><i class="fa fa-question"></i>Ask a Buddy?</a></li>';
+                                ?>                                
+                                <li><a href="#" class="sidebar-link" onclick="toggleModal('create-post')"><i class="fa fa-question"></i>Ask a Buddy?</a></li>
+                                <?php
                                     }
                                 ?>
 </ul>
