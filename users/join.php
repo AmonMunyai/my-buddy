0cadbb70b3a11a74785e080b84d7632b554cfb8b
AmonMunyai <Amonmunyai11@gmail.com>
2023-04-29 03:39:09 +0200

    Added update to user avatar when creating new account

--- users/join.php
+++ users/join.php
@@ -1,17 +1,22 @@
 <?php
-    if (isset($_POST['join-submit'])) {
-        require_once "../core/init.php";
+    require_once "../core/init.php";
 
-        $user_firstname = $_POST["firstname"];
-        $user_lastname = $_POST["lastname"];
-        $user_email = $_POST["email"];
+    if (isset($_POST['join-submit'])) {
+
+        $user_firstname = strtolower($_POST["firstname"]);
+        $user_lastname = strtolower($_POST["lastname"]);
+        $user_email = strtolower($_POST["email"]);
         $user_password = $_POST["password"];
+        $user_avatar = ROOT_URL . "/public/assets/images/blank-profile-picture.png";
         
         if (!filter_var($user_email, FILTER_VALIDATE_EMAIL)) {
             header("Location: " . ROOT_URL . "/index.php?error=invalidmail&email=".$user_email);
             exit();
         } elseif (!preg_match("/^[a-zA-Z0-9]*$/", $user_firstname)) {
-            header("Location: " . ROOT_URL . "/index.php?error=invalidusername&username=".$user_lastname);
+            header("Location: " . ROOT_URL . "/index.php?error=invalidfirstname&lastname=".$user_lastname);
+            exit();
+        } elseif (!preg_match("/^[a-zA-Z0-9]*$/", $user_lastname)) {
+            header("Location: " . ROOT_URL . "/index.php?error=invalidlastname&firstname=".$user_firstname);
             exit();
         } else {
             $sql = "SELECT * FROM user_account WHERE user_email=?";
@@ -28,14 +33,14 @@
                     header("Location: " . ROOT_URL . "/index.php?error=mailalreadytaken");
                     exit();    
                 } else {
-                    $sql = "INSERT INTO user_account (user_firstname, user_lastname, user_email, user_password) VALUES (?, ?, ?, ?)";
+                    $sql = "INSERT INTO user_account (user_firstname, user_lastname, user_email, user_password, user_avatar) VALUES (?, ?, ?, ?, ?)";
                     $stmt = mysqli_stmt_init($connection);
                     if (!mysqli_stmt_prepare($stmt, $sql)) {
                         header("Location: " . ROOT_URL . "/index.php?error=sqlerror");
                         exit();
                     } else {
                         $hash_password = password_hash($user_password, PASSWORD_DEFAULT);
-                        mysqli_stmt_bind_param($stmt, "ssss", $user_firstname, $user_lastname, $user_email, $hash_password);
+                        mysqli_stmt_bind_param($stmt, "sssss", $user_firstname, $user_lastname, $user_email, $hash_password, $user_avatar);
                         mysqli_stmt_execute($stmt);
                         header("Location: " . ROOT_URL . "/index.php?signup=success");
                         exit();
