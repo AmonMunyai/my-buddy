4fd534638cab4cad6c5bfa15a3664dee8a5caee8
AmonMunyai <Amonmunyai11@gmail.com>
2023-06-04 19:21:08 +0200

    Update about redirect to developers page

--- shared/footer.php
+++ shared/footer.php
@@ -3,11 +3,11 @@
     <!-- footer -->
     <footer class="align-center">
         <div class="footer-wrapper">
-            <ul class="footer_legal-list align-center text-size-small">
+            <ul class="footer_legal-list text-size-small">
                 <li class="footer_credit-text">&copy; University of Johannesburg</li>
                 <li><a href="#" class="footer_legal-link">Privacy Policy</a></li>
                 <li><a href="#" class="footer_legal-link">Terms of Service</a></li>
-                <li><a href="#" class="footer_legal-link">About</a></li>
+                <li><a href="<?php echo ROOT_URL; ?>/developers.php" class="footer_legal-link">About</a></li>
                 <li class="footer_credit-text">
                     powered by
                     <a href="https://www.uj.ac.za/" target="_blank" class="footer_legal-link">UJ</a>
