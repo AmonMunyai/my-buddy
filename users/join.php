76f347fa1f1a92c84607318756bd20a3a973152b
AmonMunyai <Amonmunyai11@gmail.com>
2023-06-04 19:15:02 +0200

    Added random api to create random user avatar when they create an account

--- users/join.php
+++ users/join.php
@@ -3,25 +3,33 @@
 
     if (isset($_POST['join-submit'])) {
 
-        $user_firstname = strtolower($_POST["firstname"]);
-        $user_lastname = strtolower($_POST["lastname"]);
+        $user_username = strtolower($_POST["username"]);
         $user_email = strtolower($_POST["email"]);
         $user_password = $_POST["password"];
 
-        $result = file_get_contents("https://random-data-api.com/api/v2/users?response_type=json");
-        $user_avatar = json_decode($result)->avatar;
-        
-        if (!filter_var($user_email, FILTER_VALIDATE_EMAIL)) {
+        if (empty($user_username) || empty($user_email) || empty($user_password)) {
+            header("Location: " . ROOT_URL . "/index.php?error=emptyfields");
+            exit();
+        } elseif (!preg_match("/^[a-zA-Z0-9.\-_$@*!]{3,30}$/", $user_username)) {
+            header("Location: " . ROOT_URL . "/index.php?error=invalidusername");
+            exit();
+        } elseif (!filter_var($user_email, FILTER_VALIDATE_EMAIL)) {
             header("Location: " . ROOT_URL . "/index.php?error=invalidmail&email=".$user_email);
             exit();
-        } elseif (!preg_match("/^[a-zA-Z0-9]*$/", $user_firstname)) {
-            header("Location: " . ROOT_URL . "/index.php?error=invalidfirstname&lastname=".$user_lastname);
+        } elseif (strlen($user_password) < 8) {
+            header("Location: " . ROOT_URL . "/index.php?error=passwordtooshort");
             exit();
-        } elseif (!preg_match("/^[a-zA-Z0-9]*$/", $user_lastname)) {
-            header("Location: " . ROOT_URL . "/index.php?error=invalidlastname&firstname=".$user_firstname);
+        } elseif (!preg_match("@[A-Z]@", $user_password)) {
+            header("Location: " . ROOT_URL . "/index.php?error=passwordnouppercase");
+            exit();
+        } elseif(!preg_match("@[a-z]@", $user_password)) {
+            header("Location: " . ROOT_URL . "/index.php?error=passwordnolowercase");
+            exit();
+        } elseif(!preg_match("@[0-9]@", $user_password)) {
+            header("Location: " . ROOT_URL . "/index.php?error=passwordnonumber");
             exit();
         } else {
-            $sql = "SELECT * FROM user_account WHERE user_email=?";
+            $sql = "SELECT * FROM user WHERE user_email=?";
             $stmt = mysqli_stmt_init($connection);
             if (!mysqli_stmt_prepare($stmt, $sql)) {
                 header("Location: " . ROOT_URL . "/index.php?error=sqlerror");
@@ -35,25 +43,32 @@
                     header("Location: " . ROOT_URL . "/index.php?error=mailalreadytaken");
                     exit();    
                 } else {
-                    $sql = "INSERT INTO user_account (user_firstname, user_lastname, user_email, user_password, user_avatar) VALUES (?, ?, ?, ?, ?)";
+
+                    $random_data_api = file_get_contents("https://random-data-api.com/api/v2/users?response_type=json");
+                    if ($random_data_api) {
+                        $user_avatar = json_decode($random_data_api)->avatar;
+                    } else {
+                        $user_avatar = ROOT_URL . "/public/assets/images/blank-profile-picture.png";
+                    }
+        
+                    $sql = "INSERT INTO user (user_username, user_email, user_password, user_avatar) VALUES (?, ?, ?, ?)";
                     $stmt = mysqli_stmt_init($connection);
                     if (!mysqli_stmt_prepare($stmt, $sql)) {
                         header("Location: " . ROOT_URL . "/index.php?error=sqlerror");
                         exit();
                     } else {
                         $hash_password = password_hash($user_password, PASSWORD_DEFAULT);
-                        mysqli_stmt_bind_param($stmt, "sssss", $user_firstname, $user_lastname, $user_email, $hash_password, $user_avatar);
+                        mysqli_stmt_bind_param($stmt, "ssss", $user_username, $user_email, $hash_password, $user_avatar);
                         mysqli_stmt_execute($stmt);
                         header("Location: " . ROOT_URL . "/index.php?signup=success");
                         exit();
                     }        
                 }
             }
+            mysqli_stmt_close($stmt);
+            mysqli_close($connection);
         }
-        mysqli_stmt_close($stmt);
-        mysqli_close($connection);
     } else {
         header("Location: " . ROOT_URL . "/index.php");
         exit();    
     }
-?>
