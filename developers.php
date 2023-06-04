e93a6a1a7cdab8e831fa3856e8978f30a65a15e5
AmonMunyai <Amonmunyai11@gmail.com>
2023-06-04 19:23:30 +0200

    Added developers page

---
+++ developers.php
@@ -0,0 +1,147 @@
+<?php
+    $title= "myBuddy - Your Ultimate Guide to Varsity Life";
+    $keywords = "";
+    $description = "";
+    require_once "./core/init.php";
+    require_once "./shared/header.php";
+?>
+    <!-- main component -->
+    <main class="main-wrapper">
+        <!-- section header -->
+        <header class="section_header">
+            <div class="padding-global">
+                <div class="container-medium">
+                    <div class="padding-section-medium">
+                        <div class="text-align-center">
+                            <h1 class="text-color-white heading-style-h1">Diliza Developers</h1>
+                            <div class="spacing-block padding-small-plus"></div>
+                            <p class="text-size-large">Our mission is to help students get familiar with student life, improve social interaction, and stay updated with campus news.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+
+        <section class="section_latest">
+            <div class="padding-global">
+                <div class="container-large">
+                    <div class="padding-section-medium is-padding-bottom-only">
+                        <div class="blog_item">
+                            <div class="blog_item-featured">
+                                <a href="#" class="blog_image-wrapper inline-block">
+                                    <img src="public/assets/images/TA-MUNYAI.jpg" class="blog_image" loading="lazy" alt>
+                                </a>
+                                <div class="blog_item-content">
+                                    <div class="blog_item-content-top">
+                                        <a href="#" class="inline-block text-color-white">
+                                            <h3 class="heading-style-h3">Front-End and Back-End Development</h3>
+                                            <div class="spacing-block padding-small"></div>
+                                            <p class="text-size-medium">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non, nam, minima numquam esse harum consectetur laudantium maxime tempore enim, dolorum voluptate beatae aut nesciunt accusamus!</p>
+                                        </a>
+                                        <div class="spacing-block padding-small-plus"></div>
+                                    </div>
+                                    <div class="blog_author-wrapper">
+                                        &bull;
+                                        <div class="blog_author-text">
+                                            <div class="text-weight-bold">TA MUNYAI</div>
+                                        </div>
+                                    </div>
+                                </div>    
+                            </div>
+                        </div>
+                        <div class="spacing-block padding-small-plus"></div>
+                        <ul class="blog_list">
+                            <li class="blog_item">
+                                <a href="#" class="blog_image-wrapper">
+                                    <img src="public/assets/images/blank-profile-picture.png" class="blog_image" loading="lazy" alt>
+                                </a>
+                                <div class="blog_item-content">
+                                    <div class="blog_item-content-top">
+                                        <a href="#" class="inline-block text-color-white">
+                                            <h3 class="heading-style-h3">Contributor</h3>
+                                            <div class="spacing-block padding-small"></div>
+                                            <p class="text-size-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit voluptatibus iure saepe non numquam veniam exercitationem, voluptatum, eaque rem vel cum. Unde omnis aliquid dolorum!</p>        
+                                        </a>
+                                        <div class="spacing-block padding-small-plus"></div>
+                                    </div>
+                                    <div class="blog_author-wrapper">
+                                        &bull;
+                                        <div class="blog_author-text">
+                                            <div class="text-weight-bold">T GEMBY</div>
+                                        </div>
+                                    </div>
+                                </div>    
+                            </li>
+                            <li class="blog_item">
+                                <a href="#" class="blog_image-wrapper">
+                                    <img src="public/assets/images/blank-profile-picture.png" class="blog_image" loading="lazy" alt>
+                                </a>
+                                <div class="blog_item-content">
+                                    <div class="blog_item-content-top">
+                                        <a href="#" class="inline-block text-color-white">
+                                            <h3 class="heading-style-h3">Contributor</h3>
+                                            <div class="spacing-block padding-small"></div>
+                                            <p class="text-size-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit voluptatibus iure saepe non numquam veniam exercitationem, voluptatum, eaque rem vel cum. Unde omnis aliquid dolorum!</p>        
+                                        </a>
+                                        <div class="spacing-block padding-small-plus"></div>
+                                    </div>
+                                    <div class="blog_author-wrapper">
+                                        &bull;
+                                        <div class="blog_author-text">
+                                            <div class="text-weight-bold">SS MHLONGO</div>
+                                        </div>
+                                    </div>
+                                </div>    
+                            </li>
+                            <li class="blog_item">
+                                <a href="#" class="blog_image-wrapper">
+                                    <img src="public/assets/images/blank-profile-picture.png" class="blog_image" loading="lazy" alt>
+                                </a>
+                                <div class="blog_item-content">
+                                    <div class="blog_item-content-top">
+                                        <a href="#" class="inline-block text-color-white">
+                                            <h3 class="heading-style-h3">Contributor</h3>
+                                            <div class="spacing-block padding-small"></div>
+                                            <p class="text-size-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit voluptatibus iure saepe non numquam veniam exercitationem, voluptatum, eaque rem vel cum. Unde omnis aliquid dolorum!</p>        
+                                        </a>
+                                        <div class="spacing-block padding-small-plus"></div>
+                                    </div>
+                                    <div class="blog_author-wrapper">
+                                        &bull;
+                                        <div class="blog_author-text">
+                                            <div class="text-weight-bold">MM MOKOENA</div>
+                                        </div>
+                                    </div>
+                                </div>    
+                            </li>
+                            <li class="blog_item">
+                                <a href="#" class="blog_image-wrapper">
+                                    <img src="public/assets/images/blank-profile-picture.png" class="blog_image" loading="lazy" alt>
+                                </a>
+                                <div class="blog_item-content">
+                                    <div class="blog_item-content-top">
+                                        <a href="#" class="inline-block text-color-white">
+                                            <h3 class="heading-style-h3">Contributor</h3>
+                                            <div class="spacing-block padding-small"></div>
+                                            <p class="text-size-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit voluptatibus iure saepe non numquam veniam exercitationem, voluptatum, eaque rem vel cum. Unde omnis aliquid dolorum!</p>        
+                                        </a>
+                                        <div class="spacing-block padding-small-plus"></div>
+                                    </div>
+                                    <div class="blog_author-wrapper">
+                                        &bull;
+                                        <div class="blog_author-text">
+                                            <div class="text-weight-bold">T MOKOTEDI</div>
+                                        </div>
+                                    </div>
+                                </div>    
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+<?php
+    require_once "./shared/modal.php";
+    require_once "./shared/footer.php";
+?>
