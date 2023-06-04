f19656770cf41e1c54e936faa546d01accc67015
AmonMunyai <Amonmunyai11@gmail.com>
2023-06-04 19:11:49 +0200

    Update sign in with username or email

--- users/sign_in.php
+++ users/sign_in.php
@@ -6,28 +6,26 @@
         $user_email = $_POST["email"];
         $user_password = $_POST["password"];
 
-        $sql = "SELECT * FROM user_account WHERE user_email=?";
+        $sql = "SELECT * FROM user WHERE user_email=? OR user_username=?";
         $stmt = mysqli_stmt_init($connection);
         if (!mysqli_stmt_prepare($stmt, $sql)) {
             header("Location: " . ROOT_URL . "/index.php?error=sqlerror");
             exit();
         } else {
-            mysqli_stmt_bind_param($stmt, "s", $user_email);
+            mysqli_stmt_bind_param($stmt, "ss", $user_email, $user_email);
             mysqli_stmt_execute($stmt);
             $result = mysqli_stmt_get_result($stmt);
             if ($row = mysqli_fetch_assoc($result)) {
                 $check_password = password_verify($user_password, $row['user_password']);
                 if ($check_password == false) {
-                    header("Location: " . ROOT_URL . "/index.php?error=incorrectpassword");
+                    header("Location: " . ROOT_URL . "/index.php?error=passwordnomatch");
                     exit();
                 } else if ($check_password == true) {
                     $_SESSION['user_id'] = $row['id'];
-                    $_SESSION['user_firstname'] = $row['user_firstname'];
-                    $_SESSION['user_lastname'] = $row['user_lastname'];
-                    $_SESSION['user_username'] = $row['user_firstname'] . $row['user_lastname'];
+                    $_SESSION['user_username'] = $row['user_username'];
                     $_SESSION['user_avatar'] = $row['user_avatar'];
 
-                    header("Location: " . ROOT_URL . "/dashboard.php?login=success");
+                    header("Location: " . ROOT_URL . "/dashboard.php?login=");
                     exit();    
                 }
             } else {
